f09f84754416e6b11644d00de8134ab28ed21737
Raeed <shksper1051993@gmail.com>
2022-09-19 23:52:26 +0300

    Update LinkedBuilder.java

--- src/main/java/Home/LinkedBuilder.java
+++ src/main/java/Home/LinkedBuilder.java
@@ -1,4 +1,38 @@
 package Home;
 
-public class LinkedBuilder {
+public class LinkedBuilder <T> {
+
+    T t;
+    private Node head;
+    private Node last;
+
+
+    public LinkedBuilder add(T nodet) {
+        this.t = nodet;
+        if (head == null) {
+            Node node = new Node<>(t);
+            head = node;
+            last = node;
+        } else {
+            Node node = new Node<>(t);
+            last.next = node;
+            last = node;
+
+        }
+        return this;
+    }
+
+    public void getAll() {
+        Node n = head;
+        while (n != null) {
+
+            System.out.println(n.val);
+            n = n.next;
+        }
+    }
+
+    public LinkedList build() {
+        return new LinkedList(head, last);
+    }
+
 }
