e8d57d4d2b2a867a6828ebf583766a2c41959390
Raeed <shksper1051993@gmail.com>
2022-09-19 23:47:33 +0300

    Update HashTable.java

--- src/main/java/Home/HashTable.java
+++ src/main/java/Home/HashTable.java
@@ -1,8 +1,26 @@
 package Home;
 
-public class HashTable {
+public class HashTable <T>{
     private int key;
     final int index=28;
     LinkedList []lists=new LinkedList[28];
+    public void add(T t){
+        this.key=t.toString().charAt(0)%index;
+        if (lists[this.key]==null){
+            lists[this.key]=new LinkedList();
+        }
+        lists[this.key].add(t);
+
+    }
+
+    public void printAll(T key) {
+        int index=key.toString().charAt(0)%this.index;
+        lists[index].getAll();
+    }
+
+
+
+
+
 
 }
