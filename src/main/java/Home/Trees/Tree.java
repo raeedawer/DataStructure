bd753b9c9dc52c15facba49bea8320cfddf90cd1
Raeed <shksper1051993@gmail.com>
2022-09-20 00:06:53 +0300

    Update  Tree.java

--- src/main/java/Home/Trees/Tree.java
+++ src/main/java/Home/Trees/Tree.java
@@ -1,4 +1,19 @@
 package Home.Trees;
 
 public interface Tree {
+    void add(int t);
+
+    void printAll();
+
+    void delete(Nodet nodet);
+
+    void delete();
+
+    static int depth(Nodet n) {
+        return 0;
+    }
+
+    static int height(Nodet n) {
+        return 0;
+    }
 }
