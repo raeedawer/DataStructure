f325998149c107e1820945d98af01ff364e428aa
Raeed <shksper1051993@gmail.com>
2022-09-19 23:49:15 +0300

    Update LinkedList.java

--- src/main/java/Home/LinkedList.java
+++ src/main/java/Home/LinkedList.java
@@ -1,4 +1,57 @@
 package Home;
 
 public class LinkedList {
+    Node head;
+    Node last;
+
+    public LinkedList(Node head, Node last) {
+        this.head = head;
+        this.last = last;
+
+    }
+
+    public LinkedList() {
+    }
+
+    @Override
+    public LinkedBuilder add(Object nodet) {
+        this.t = nodet;
+        if (head == null) {
+            Node node = new Node<>(t);
+            head = node;
+            last = node;
+        } else {
+            Node node = new Node<>(t);
+            last.next = node;
+            last = node;
+
+        }
+        return this;
+    }
+
+
+    public void getAll() {
+        Node n = head;
+        while (n != null) {
+
+            System.out.println(n.val);
+            n = n.next;
+        }
+    }
+
+    public void delete() {
+        if (head != null) {
+
+            head = head.next;
+
+        }
+
+
+    }
+
+
+    public static LinkedBuilder Builder() {
+
+        return new LinkedBuilder();
+    }
 }
